202d34002e3ad4f61d5d9a43eff4c0ee3dcde894
cremerfc <cremerfc@users.noreply.github.com>
2023-12-11 17:17:38 -0500

    Create variables.tf

---
+++ {{ cookiecutter.project_slug }}/variables.tf
@@ -0,0 +1,17 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+variable "region" {
+  description = "AWS region"
+  default     = "{{ cookiecutter.region }}"
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default     = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "EC2 instance name"
+  default     = "{{ cookiecutter.instance_name }}"
+}
